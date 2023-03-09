9fc4221af966ad286d924408e627b14c9efb4cb2
devanshsinghvaluecoders <devansh.singh@mail.valuecoders.com>
2023-03-09 16:32:31 +0530

    first commit

---
+++ pages/LandingPage.js
@@ -0,0 +1,104 @@
+import { TextField, Card, CardContent, Button } from '@mui/material';
+import Docxtemplater from 'docxtemplater';
+import PizZip from 'pizzip';
+import React, { useState } from 'react';
+
+function LandingPage() {
+  const [data, setData] = useState({
+    recruiter_name: '',
+    recruiter_email: '',
+    your_email: '',
+    position: '',
+    company_name: '',
+    company_description: '',
+  });
+  console.log(data);
+  const handelChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setData((prevState) => ({ ...prevState, [name]: value }));
+  };
+  return (
+    <Card>
+      <CardContent>
+        <TextField
+          onChange={handelChange}
+          label='Outlined'
+          variant='outlined'
+          name='recruiter_name'
+          label="Recruiter's name"
+        />
+        <TextField
+          onChange={handelChange}
+          label='Outlined'
+          variant='outlined'
+          name='recruiter_email'
+          label="Recruiter's Email"
+        />
+        <TextField
+          onChange={handelChange}
+          label='Outlined'
+          variant='outlined'
+          name='your_email'
+          label='Your Email Address'
+        />
+        <TextField
+          onChange={handelChange}
+          label='Outlined'
+          variant='outlined'
+          name='position'
+          label='Position Applying for'
+        />
+        <TextField
+          onChange={handelChange}
+          label='Outlined'
+          variant='outlined'
+          name='company_name'
+          label='Company Name'
+        />
+        <TextField
+          onChange={handelChange}
+          label='Outlined'
+          label='Company Description'
+          variant='outlined'
+          name='company_description'
+        />
+
+        <Button variant='contained' component='label'>
+          Upload File
+          <input
+            type='file'
+            hidden
+            onChange={(e) => {
+              const reader = new FileReader();
+              reader.onload = async (e) => {
+                const content = e.target.result;
+                var doc = new Docxtemplater(new PizZip(content), {
+                  delimiters: {
+                    start: '12op1j2po1j2poj1po',
+                    end: 'op21j4po21jp4oj1op24j',
+                  },
+                });
+                var text = doc.getFullText();
+                console.log(text);
+              };
+              reader.readAsBinaryString(e.target.files[0]);
+            }}
+          />
+        </Button>
+
+        <Button
+          onClick={() => {
+            fetch('/api/Chatgpt', {
+              method: 'GET',
+            });
+          }}
+        >
+          Send
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default LandingPage;
---
+++ pages/api/Chatgpt.js
@@ -0,0 +1,30 @@
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    // const prompt = `My name is ${applicantName}. I want to work for ${companyName}, they are ${companyDescription}
+    // I am applying for the job ${jobTitle}. I have been working before for: ${remainderText()}
+    // And I have used the technologies such as ${technologies}
+    // I want to cold email ${recruiterName} my resume and write why I fit for the company.
+    // Can you please write me the email in a friendly voice, not offical? without subject, maximum 300 words and say in the end that my CV is attached.`;
+
+    const response = await fetch(
+      'https://api.openai.com/v1/engines/text-davinci-003/completions',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: process.env.Token,
+        },
+        body: JSON.stringify({
+          prompt: `write a professional comprehensive review in breif of boat object around 1000 words in professional tone which is more descriptive based on attributes such as`,
+          temperature: 0.6,
+          max_tokens: 350,
+          top_p: 1,
+          frequency_penalty: 1,
+          presence_penalty: 1,
+        }),
+      }
+    );
+    const data = await response.json();
+    return res.status(200).json({ text: data.choices[0].text });
+  }
+}
