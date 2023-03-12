dc1405093ebb627ced14ae086ef3fbaaebcb6530
devanshsinghvaluecoders <devansh.singh@mail.valuecoders.com>
2023-03-12 16:22:40 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
+// import { Inter } from 'next/font/google';
+// import styles from '@/styles/Home.module.css';
 import Contianer from '../Components/Container';
 import { Grid } from '@mui/material';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   console.log('test env', process.env.name);
